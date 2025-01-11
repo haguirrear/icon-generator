37d61e98f33a07aa478a547429097083e91383cc
Hector Aguirre <hector.aguirre.arista@gmail.com>
2025-01-11 01:41:44 -0500

    add url for project

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,4 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
@@ -8,45 +7,49 @@ export default $config({
       home: "aws",
       providers: {
         aws: {
-          profile: input.stage === "production" ? "icon-production" : "icon-dev"
-        }
-      }
+          profile:
+            input.stage === "production" ? "icon-production" : "icon-dev",
+        },
+        cloudflare: "5.47.0",
+      },
     };
   },
   async run() {
-
     // Secrets
-    const DATABASE_URL = new sst.Secret("DATABASE_URL")
-    const MERCADO_PAGO_ACCESS_TOKEN = new sst.Secret("MERCADO_PAGO_ACCESS_TOKEN")
-    const TOGETHER_API_KEY = new sst.Secret("TOGETHER_API_KEY")
-    const GOOGLE_CLIENT_ID = new sst.Secret("GOOGLE_CLIENT_ID")
-    const GOOGLE_CLIENT_SECRET = new sst.Secret("GOOGLE_CLIENT_SECRET")
-    const SECRET_KEY = new sst.Secret("SECRET_KEY", "S3Critkey")
-
+    const DATABASE_URL = new sst.Secret("DATABASE_URL");
+    const MERCADO_PAGO_ACCESS_TOKEN = new sst.Secret(
+      "MERCADO_PAGO_ACCESS_TOKEN",
+    );
+    const TOGETHER_API_KEY = new sst.Secret("TOGETHER_API_KEY");
+    const GOOGLE_CLIENT_ID = new sst.Secret("GOOGLE_CLIENT_ID");
+    const GOOGLE_CLIENT_SECRET = new sst.Secret("GOOGLE_CLIENT_SECRET");
+    const SECRET_KEY = new sst.Secret("SECRET_KEY", "S3Critkey");
     // Env Vars
     const hostMap = new Map([
       ["production", ""],
-      ["dev", "d1w4cpv7i8zop8.cloudfront.net"]
-    ])
+      ["dev", "d1w4cpv7i8zop8.cloudfront.net"],
+    ]);
     const siteUrlMap = new Map([
       ["production", ""],
       ["dev", `https://${hostMap.get("dev")}`],
-    ])
-
-
+    ]);
     // Infrastructure
-
     const bucket = new sst.aws.Bucket("AssetsBucket", {
-      access: "public"
-    })
-
-    const email = $app.stage === "haguirrear"
-      ? sst.aws.Email.get("EmailProvider", "hector.aguirre.arista@gmail.com")
-      : new sst.aws.Email("EmailProvider", {
-        sender: "hector.aguirre.arista@gmail.com"
-      })
-
+      access: "public",
+    });
+    const email =
+      $app.stage === "haguirrear"
+        ? sst.aws.Email.get("EmailProvider", "hector.aguirre.arista@gmail.com")
+        : new sst.aws.Email("EmailProvider", {
+          sender: "hector.aguirre.arista@gmail.com",
+        });
     const remixApp = new sst.aws.Remix("RemixApp", {
+      domain: {
+        // name: $app.stage == "production" ? "therevol.co" : "dev.therevol.co",
+        name: "therevol.co",
+        redirects: ["www.therevol.co"],
+        dns: sst.cloudflare.dns(),
+      },
       link: [
         bucket,
         email,
@@ -58,37 +61,36 @@ export default $config({
         SECRET_KEY,
       ],
       environment: {
-        NODE_ENV: ["dev", "production"].includes($app.stage) ? "production" : "dev",
+        NODE_ENV: ["dev", "production"].includes($app.stage)
+          ? "production"
+          : "dev",
         IS_DEV: $dev.toString(),
-        SITE_URL: $dev ? "http://localhost:5173" : siteUrlMap.get($app.stage) || "",
+        SITE_URL: $dev
+          ? "http://localhost:5173"
+          : siteUrlMap.get($app.stage) || "",
         HOST: $dev ? "localhost" : hostMap.get($app.stage) || "",
         DEFAULT_CREDIT_PRICE: "10",
         DEFAULT_CREDIT_PER_IMAGE: "1",
-      }
+      },
     });
-
     // dev
     new sst.x.DevCommand("Studio", {
       link: [DATABASE_URL],
       dev: {
         autostart: true,
-        command: "pnpm exec drizzle-kit studio"
-      }
-    })
-
-
+        command: "pnpm exec drizzle-kit studio",
+      },
+    });
     new sst.x.DevCommand("Migrate", {
       link: [DATABASE_URL],
       dev: {
         autostart: false,
-        command: "pnpm exec drizzle-kit push"
-      }
-    })
-
+        command: "pnpm exec drizzle-kit push",
+      },
+    });
     return {
       remixAppUrl: remixApp.url,
-      EmailProvider: email.getSSTLink().properties.sender
-    }
+      EmailProvider: email.getSSTLink().properties.sender,
+    };
   },
 });
-
